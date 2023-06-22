0129365b259454781e07edee334ab79fd8e3e315
algorrad <36845580+algorrad@users.noreply.github.com>
2023-06-22 18:46:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,3 +9,24 @@ def convert_char_to_int(char):
 char = input("Введите символ: ")
 result = convert_char_to_int(char)
 print(result)
+
+import sqlite3
+
+# создаем подключение
+con = sqlite3.connect("students.db")
+
+# получаем курсор
+cursor = con.cursor()
+cursor.execute("SELECT DATA FROM zachetka")
+results = cursor.fetchall()
+
+# Вывод результатов
+for row in results:
+    data = row[0]  # Получение значения колонки "DATA"
+    print(data)
+
+# Закрытие курсора
+cursor.close()
+
+# Закрытие соединения
+con.close()
