0561ebfa7734575815048a187967824c3e1f332f
algorrad <36845580+algorrad@users.noreply.github.com>
2023-06-22 18:25:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def convert_char_to_int(char):
     if char.isdigit():  # Проверяем, является ли символ цифрой
         return int(char)  # Преобразуем символ в целое число
     else:
-        return output  # Если символ не является цифрой, возвращаем None или можно выбрать другое значение по умолчанию
+        return output
 #char = input("Введите символ: ")
 #convert_char_to_int(char)
 char = input("Введите символ: ")
